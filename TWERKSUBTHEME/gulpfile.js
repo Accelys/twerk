1b450608cf0818eaaac5c4767f538ea45cc64a0a
Cédric Albrecht <cedric@webla.fr>
2018-04-18 17:02:55 +0200

    WIP on gulp tasks

--- TWERKSUBTHEME/gulpfile.js
+++ TWERKSUBTHEME/gulpfile.js
@@ -8,10 +8,39 @@ var $ = require('gulp-load-plugins')();
 var del = require('del');
 var autoprefixer = require('autoprefixer');
 
+
+/**
+ * @task eslint
+ * ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code
+ * abort calling task on error
+ */
+// @TODO change js task (from fortytwo) to use eslint instead of jscs
+/**
+ * @task js
+ *
+ */
+gulp.task('js', function () {
+  var stream = gulp.src(['static/js/**/*.js'])
+  .pipe($.jscs({fix: true}))
+  .pipe($.jscs.reporter())
+  .pipe(gulp.dest('./static/js'))
+  .on('error', $.util.log);
+
+  if (config.enable_livereload) {
+    stream.pipe($.livereload());
+  }
+
+  return stream;
+});
+
+
+
+
 /**
  * @task sass-lint
  * Lint sass, abort calling task on error
  */
+// @TODO change sass-lint task (from fortytwo) to a `stylelint` task
 gulp.task('sass-lint', function () {
   return gulp.src('sass/**/*.s+(a|c)ss')
   .pipe($.sassLint({configFile: '.sass-lint.yml'}))
@@ -25,9 +54,11 @@ gulp.task('sass-compile', ['sass-lint'], function () {
     autoprefixer: require('autoprefixer'),
   };
   var pcProcess = [
+    // Parse CSS and add vendor prefixes to CSS rules using values from the Can I Use website
     pcPlug.autoprefixer({
       browsers: ['> 5%', 'last 2 versions', 'safari 8', 'IE 10', 'IE 11']
     }),
+    // Pack same CSS media query rules into one using PostCSS
     pcPlug.mqpacker()
   ];
 
